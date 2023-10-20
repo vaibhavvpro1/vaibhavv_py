1c3e73ed45611527cae8b203b53ec73b20e06551
vaibhavvpro1 <146124931+vaibhavvpro1@users.noreply.github.com>
2023-10-20 12:59:59 +0530

    Add files via upload

---
+++ APP.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.set_page_config(page_title="VGS Adventure India",page_icon="🌎")
+st.balloons()
+st.header("Vaibhav Borse 😎")
+st.sidebar.title("Smartbyte Computer Education")
+st.sidebar.text_input("Mail address")
+st.sidebar.text_input("Password")
+st.sidebar.button('Click here')
+st.sidebar.radio("Your age between",["5-10","11-15","16-20","21-25"])
+st.title("Bar chart")
+data=pd.DataFrame(np.random.randn(50,2),columns=["x","y"])
+st.bar_chart(data)
+st.title("Line chart")
+st.line_chart(data)
+st.title("Area chart")
+st.area_chart(data)
+st.sidebar.file_uploader("Upload your file/folder")
+st.chat_input("Type here")
+st.status("Working")
+st.markdown("This site is devloped by vaibhav borse")
