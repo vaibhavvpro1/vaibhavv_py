97f98bcbb168457d7bfc2634d306eb19e7fae651
vaibhavvpro1 <146124931+vaibhavvpro1@users.noreply.github.com>
2023-10-28 15:01:34 +0530

    Update APP.py

--- APP.py
+++ APP.py
@@ -20,3 +20,15 @@ st.area_chart(data)
 st.sidebar.file_uploader(":green[Upload your file/folder]")
 st.chat_input("Type here")
 st.status("Working")
+df = pd.DataFrame(columns=['name','age','color'])
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+config = {
+    'name' : st.column_config.TextColumn('Full Name (required)', width='large', required=True),
+    'age' : st.column_config.NumberColumn('Age (years)', min_value=0, max_value=122),
+    'color' : st.column_config.SelectboxColumn('Favorite Color', options=colors)
+}
+
+result = st.data_editor(df, column_config = config, num_rows='dynamic')
+
+if st.button('Get results'):
+    st.write(result)
